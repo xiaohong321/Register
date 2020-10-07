9a5f51ed4fedf81965286313eae6984bb87f11f3
xiaohongsheng <1090176294@qq.com>
2020-10-07 16:53:01 +0800

    test kill.sh

--- src/main/java/com/xiao/register/controller/HelloController.java
+++ src/main/java/com/xiao/register/controller/HelloController.java
@@ -15,6 +15,6 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello(){
         System.out.println("this is a hello test");
-        return "Hello, world my jenkins";
+        return "Hello, world my jenkins ,use kill test.sh success";
     }
 }
